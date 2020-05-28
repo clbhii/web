29132dc0e215e56f6d76a0ab308a672529e31b36
clbhii <clbhii@163.com>
2020-05-28 08:55:55 +0800

    react

---
+++ web/reactweb/src/Hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { DatePicker } from 'antd'
+import 'antd/dist/antd.css';
+class Hello extends React.Component{
+    render() {
+        return (
+            <div>hello world11<DatePicker /></div>
+        )
+    }
+}
+export  default  Hello;
